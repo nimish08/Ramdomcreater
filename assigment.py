3d833b325c14bf3230faa9897bd5254322736f9c
Nimish Mittal <mittalnimish8@gmail.com>
2020-12-12 14:26:46 +0530

    Update assigment.py

--- assigment.py
+++ assigment.py
@@ -8,12 +8,7 @@ background-size: cover;
 }
 </style>
 '''
-from PIL import Image
-image = Image.open('123456.jpg')
 
-st.image(image, caption='UEFA Campion league', imgage_align='left', width="100", height="100" ,
-         use_column_width=True)
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 
